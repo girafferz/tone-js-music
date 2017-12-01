89d2897ec7b97da4506f8049aaeb3ad68ae27a3b
hiroyukishirai <hirshira@exmed.io>
2017-12-01 19:23:34 +0900

    loop

--- src/index.js
+++ src/index.js
@@ -1,40 +1,53 @@
 //create a synth and connect it to the master output (your speakers)
-console.log('-----------------')
 var synth = new Tone.Synth().toMaster();
-console.log('-----------------')
-
-console.log('-----------------')
 //play a middle 'C' for the duration of an 8th note
-console.log('-----------------')
-console.log('-----------------')
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 const fuga = async () => {
-    console.log('---a---');
     synth.triggerAttackRelease("C4", "8n");
 };
 
 const hoge = async () => {
-    console.log('---b---');
     synth.triggerAttackRelease('C5', '16n');
     };
 
 const beep = async (key, num) => {
-    console.log('---b---');
     synth.triggerAttackRelease(key, num.toString() + 'n');
     };
 
 
-const init = async () => {
-    await fuga();
-    await sleep(1000);
-    await hoge();
-    await sleep(1000);
+const coin = async () => {
     for(var i=1; i<10;i++) {
         await beep('C'+(i % 10).toString(), i);
         await sleep(10 + Math.random() * 100);
         console.log(10 + Math.random() * 100);
+    }
+}
+
+const coin2 = async () => {
+    let rec = [];
+    let rnd = 30;
+    for(var i=1; i<10;i++) {
+        let hoge = 'C'+(Math.floor(Math.random()*20) % 7).toString();
+        await beep(hoge, i);
+        var sleepVal = 1 + Math.random() * rnd
+        await sleep(sleepVal);
+        rec.push({key:hoge, val:sleepVal});
+    }
+    console.log(rec)
+}
+
+
+const init = async () => {
+    //await fuga();
+    //await sleep(1000);
+    //await hoge();
+    //await sleep(1000);
+    //await coin();
+    for(var i=0; i<1000;i++) {
+      await coin2();
+      await sleep(150);
     }
 };
 
